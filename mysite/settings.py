491e4c22533a0d30bed52287561fcd3a9841ad77
AleksandrYulgu <yulgu987@gmail.com>
2022-03-27 17:29:39 +0300

    Sent_mail

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'yulgu987@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sigil.11'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
